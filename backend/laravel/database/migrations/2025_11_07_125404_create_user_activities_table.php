819e6ecf6835d75095916f8e5174f2265c67be2c
gabrielleles18 <gabriel.leles18@gmail.com>
2025-11-07 11:01:51 -0300

    add migration

---
+++ backend/laravel/database/migrations/2025_11_07_125404_create_user_activities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('activity', ['watched_video', 'practiced_sentence']);
+            $table->foreignId('video_id')->constrained('videos')->nullable();
+            $table->foreignId('sentence_id')->constrained('sentences')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_activities');
+    }
+};
